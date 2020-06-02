61acb436f399adc099bafd508ad67b5a88a0300d
mbade1 <mbade314@gmail.com>
2020-06-01 22:54:23 -0500

    corrected require files

--- lib/blue_bell_flavors.rb
+++ lib/blue_bell_flavors.rb
@@ -2,10 +2,10 @@ require 'pry'
 require 'open-uri'
 require 'nokogiri'
 
-require_relative "../lib/blue_bell_flavors/blue_bell_ice_cream.rb"
-require_relative "../lib/blue_bell_flavors/cli.rb"
-require_relative "../lib/blue_bell_flavors/scraper.rb"
-require_relative "../lib/blue_bell_flavors/version.rb"
+require_relative "blue_bell_flavors/blue_bell_ice_cream"
+require_relative "lib/blue_bell_flavors/cli"
+require_relative "lib/blue_bell_flavors/scraper"
+require_relative "lib/blue_bell_flavors/version"
 
 module BlueBell
 end
